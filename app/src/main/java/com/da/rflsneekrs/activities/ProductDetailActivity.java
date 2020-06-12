00648aedad670951638b81f1821d3bf5429a53fa
Davide Agosti <davide.a@davideagosti.info>
2020-06-12 16:00:40 +0100

    Add share button on products

--- app/src/main/java/com/da/rflsneekrs/activities/ProductDetailActivity.java
+++ app/src/main/java/com/da/rflsneekrs/activities/ProductDetailActivity.java
@@ -3,20 +3,27 @@ package com.da.rflsneekrs.activities;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.da.rflsneekrs.R;
 import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
+
+import java.util.Objects;
 
 public class ProductDetailActivity extends AppCompatActivity {
 
   private FirebaseDatabase fbDatabase;
   private ImageView imgProduct;
   private TextView txtViewName, txtViewBrand, txtViewSecondaryName, txtViewSecondaryBrand, txtViewPrice, txtViewDescription;
+  private ImageButton imgShare, imgFavorite;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +39,7 @@ public class ProductDetailActivity extends AppCompatActivity {
     // we need to send product detail data to this activity first ...
     // now we can get product data
     // ImageView on activity product detail
-    String productImage = getIntent().getExtras().getString("image");
+    String productImage = Objects.requireNonNull(getIntent().getExtras()).getString("image");
     Picasso.get().load(productImage).into(imgProduct);
     // TextView on activity product detail
     String productName = getIntent().getExtras().getString("name");
@@ -47,6 +54,15 @@ public class ProductDetailActivity extends AppCompatActivity {
     txtViewPrice.setText(productPrice);
     String productDescription = getIntent().getExtras().getString("description");
     txtViewDescription.setText(productDescription);
+
+    imgShare.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+        sharingIntent.setType("text/plain");
+        startActivity(Intent.createChooser(sharingIntent, "Share via"));
+      }
+    });
 
     // Check if the action bar is displayed if so, set the back arrow and the title.
     if (getSupportActionBar() != null) {
@@ -63,6 +79,8 @@ public class ProductDetailActivity extends AppCompatActivity {
     txtViewSecondaryBrand = findViewById(R.id.product_detail_brand_bottom);
     txtViewPrice = findViewById(R.id.product_detail_price);
     txtViewDescription = findViewById(R.id.product_detail_description);
+    imgShare = findViewById(R.id.share_btn_detail);
+    imgFavorite = findViewById(R.id.favorite_btn_detail);
   }
 
   // This method is used to the back arrow on application bar
