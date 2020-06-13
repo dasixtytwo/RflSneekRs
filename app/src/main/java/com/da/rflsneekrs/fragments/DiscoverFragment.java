d9ba69474b90e94226c3b5a94c01b20aa97e26d6
Davide Agosti <davide.a@davideagosti.info>
2020-06-13 13:55:01 +0100

    Some fix

--- app/src/main/java/com/da/rflsneekrs/fragments/DiscoverFragment.java
+++ app/src/main/java/com/da/rflsneekrs/fragments/DiscoverFragment.java
@@ -2,27 +2,14 @@ package com.da.rflsneekrs.fragments;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.da.rflsneekrs.R;
-import com.da.rflsneekrs.adapters.ProductAdapter;
-import com.da.rflsneekrs.models.Product;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
