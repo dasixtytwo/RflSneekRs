af8bcc9a868ad309df816d807d97969900814fa8
Davide Agosti <davide.a@davideagosti.info>
2020-06-19 14:20:30 +0100

    Update Setting Activity

--- app/src/main/java/com/da/rflsneekrs/activities/SettingsActivity.java
+++ app/src/main/java/com/da/rflsneekrs/activities/SettingsActivity.java
@@ -1,13 +1,18 @@
 package com.da.rflsneekrs.activities;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
 
 import com.da.rflsneekrs.R;
 import com.google.android.material.tabs.TabLayout;
@@ -18,12 +23,44 @@ public class SettingsActivity extends AppCompatActivity {
   Button logout;
   FirebaseAuth auth;
   TabLayout tabLayout;
+  ImageButton uploadImg;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_settings);
     auth = FirebaseAuth.getInstance();
+    uploadImg = findViewById(R.id.upload_pictures_iv);
+
+    uploadImg.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        AlertDialog alertDialog = new AlertDialog.Builder(SettingsActivity.this).create();
+        alertDialog.setTitle("Alert");
+        alertDialog.setMessage("Alert message to be shown");
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+            new DialogInterface.OnClickListener() {
+              public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+              }
+            });
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "ACCEPT",
+            new DialogInterface.OnClickListener() {
+              @Override
+              public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "Button ACCEPT presses!", Toast.LENGTH_LONG).show();
+              }
+            });
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "DECLINE",
+            new DialogInterface.OnClickListener() {
+              @Override
+              public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "Button DECLINE presses!", Toast.LENGTH_LONG).show();
+              }
+            });
+        alertDialog.show();
+      }
+    });
 
     if(auth.getCurrentUser() == null){
       Intent intent = new Intent(SettingsActivity.this, MainUnlogActivity.class);
